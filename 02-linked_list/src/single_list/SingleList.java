2c1bf7a025b6a985ff49b0f2533b596eae9782b4
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2021-02-24 18:32:12 +0800

    0

---
+++ 02-linked_list/src/single_list/SingleList.java
@@ -0,0 +1,164 @@
+package single_list;
+
+public class SingleList {
+	/**
+	 * 元素的数量
+	 */
+	private int size;
+	/*
+	 * 所有的元素
+	 */
+	private E[] elements;
+	
+	private static final int DEFAULT_CAPACITY= 10;
+	private static final int ELEMENT_NOT_FOUND = -1;
+	
+	/*
+	 * 构造函数
+	 */
+	public SingleList () {
+		this(DEFAULT_CAPACITY);
+	}
+	
+	public SingleList (int capaticy) {
+		capaticy = (capaticy < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capaticy;
+		elements = (E[]) new Object[capaticy];
+	}
+	/*
+	 * 清除所有元素
+	 */
+	public void clear() {
+		first = null;
+		size = 0;
+	}
+	/**
+	 * 元素的数量
+	 * @return
+	 */
+	public int size() {
+		return size;
+	}
+	/**
+	 * 是否为空
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	/**
+	 * 是否包含某个元素
+	 * @param element
+	 * @return
+	 */
+	public boolean contains(E element) {
+		return indexOf(element) != ELEMENT_NOT_FOUND;
+	}
+	/**
+	 * 添加元素到尾部
+	 * @param element
+	 */
+	public void add(E element) {
+		add(size,element);
+	}
+	/**
+	 * 获取index位置的元素
+	 * @param index
+	 * @return
+	 */
+	public E get(int index) {
+		check(index);
+		return elements[index];
+	}
+	/**
+	 * 设置index位置的元素
+	 * @param index
+	 * @param element
+	 * @return 原来的元素ֵ
+	 */
+	public E set(int index,E element) {
+		check(index);
+		E old = elements[index];
+		elements[index] = element;
+		return old;
+	}
+	/**
+	 * 在index位置插入一个元素
+	 * @param index
+	 * @param element
+	 */
+	public void add(int index,E element) {
+		checkForAdd(index);
+		ensureCapacity(size + 1);
+		
+		for (int i = size - 1; i >= index; i--) {
+			elements[i + 1] = elements[i];
+		}
+		elements[index] = element;
+		size++;
+	}
+	/**
+	 * 删除index位置的元素
+	 * @param index
+	 * @return
+	 */
+	public E remove(int index) {
+		check(index);
+		E old = elements[index];
+		
+		for (int i = index + 1; i <= size - 1; i++) {
+			elements[i - 1] = elements[i];
+		}
+		elements[--size] = null;
+		return old;
+	}
+	/**
+	 * 查看元素的索引
+	 * @param element
+	 * @return
+	 */
+	public int indexOf(E element) {
+		if(element == null) {
+			for(int i = 0; i < size; i++) {
+				if(elements[i] == null) return i;
+			}
+		}else {
+			for(int i = 0; i < size; i++) {
+				if(element.equals(elements[i])) return i;
+			}
+		}
+		return ELEMENT_NOT_FOUND;
+	}
+	/**
+	 * 保证要有capacity的容量
+	 * @param capacity
+	 */
+	private void ensureCapacity(int capacity) {
+		int oldCapacity = elements.length;
+		if(oldCapacity >= capacity)return;
+		// 新容量为旧容量的1.5倍
+		int newCapacity = oldCapacity + (oldCapacity >> 1);
+		E[] newElements = (E[]) new Object[newCapacity];
+		
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[i];
+		}
+		elements = newElements;
+	}
+	
+	private void outOfBounds(int index) {
+		throw new IndexOutOfBoundsException("Index:" + index + ",Size:" + size);
+	}
+	
+	private void check(int index) {
+		if(index < 0 || index >= size) {
+			outOfBounds(index);
+		}
+	}
+	
+	private void checkForAdd(int index) {
+		if(index < 0 || index > size) {
+			outOfBounds(index);
+		}
+	}
+
+}
