3721a7198929865c777fa292d55ddc65e53d77ee
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2021-03-09 21:51:53 +0800

    set

---
+++ 02-linked_list/src/circle_list/Main.java
@@ -0,0 +1,13 @@
+package circle_list;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CircleList circle = new CircleList();
+        circle.add("wenya");
+        circle.add(521);
+        circle.add("**");
+        System.out.println(circle);
+
+    }
+}
